a8fd8cbcd3794a6c60d1f646d68c99593b238839
Sun Lee <36293619+appano1@users.noreply.github.com>
2024-01-13 15:03:23 +0900

    feat: Support glob pattern in options.project (#84)
    * feat: Support glob pattern in options.project
    
    * Create slow-jobs-confess.md
    
    ---------
    
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>

--- src/utils/get-project-config-files.ts
+++ src/utils/get-project-config-files.ts
@@ -1,16 +1,26 @@
 import type { ParserOptions } from "@typescript-eslint/parser";
 import fs from "fs";
+import { glob } from "glob";
 import path from "path";
+
+function syncWithGlob(pattern: string, cwd: string): string[] {
+  return glob
+    .sync(pattern, { cwd })
+    .map((filePath) => path.resolve(cwd, filePath));
+}
 
 export function getProjectConfigFiles(options: ParserOptions): string[] {
   const tsconfigRootDir =
     typeof options.tsconfigRootDir === "string"
       ? options.tsconfigRootDir
       : process.cwd();
+
   if (options.project !== true) {
     return Array.isArray(options.project)
-      ? options.project
-      : [options.project!];
+      ? options.project.flatMap((projectPattern: string) =>
+          syncWithGlob(projectPattern, tsconfigRootDir),
+        )
+      : syncWithGlob(options.project!, tsconfigRootDir);
   }
 
   let directory = path.dirname(options.filePath!);
