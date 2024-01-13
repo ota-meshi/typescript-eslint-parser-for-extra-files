9db71356058260faec1015fe5575fb2ec6f88adc
ota-meshi <otameshiyo23@gmail.com>
2024-01-13 15:30:39 +0900

    fix: behavior when using glob in `project`

--- src/utils/get-project-config-files.ts
+++ src/utils/get-project-config-files.ts
@@ -1,26 +1,17 @@
-import type { ParserOptions } from "@typescript-eslint/parser";
 import fs from "fs";
-import { glob } from "glob";
 import path from "path";
 
-function syncWithGlob(pattern: string, cwd: string): string[] {
-  return glob
-    .sync(pattern, { cwd })
-    .map((filePath) => path.resolve(cwd, filePath));
-}
-
-export function getProjectConfigFiles(options: ParserOptions): string[] {
-  const tsconfigRootDir =
-    typeof options.tsconfigRootDir === "string"
-      ? options.tsconfigRootDir
-      : process.cwd();
-
+export function getProjectConfigFiles(
+  options: Readonly<{
+    project: string[] | string | true | null | undefined;
+    tsconfigRootDir: string;
+    filePath?: string;
+  }>,
+): string[] {
   if (options.project !== true) {
     return Array.isArray(options.project)
-      ? options.project.flatMap((projectPattern: string) =>
-          syncWithGlob(projectPattern, tsconfigRootDir),
-        )
-      : syncWithGlob(options.project!, tsconfigRootDir);
+      ? options.project
+      : [options.project!];
   }
 
   let directory = path.dirname(options.filePath!);
@@ -34,9 +25,12 @@ export function getProjectConfigFiles(options: ParserOptions): string[] {
 
     directory = path.dirname(directory);
     checkedDirectories.push(directory);
-  } while (directory.length > 1 && directory.length >= tsconfigRootDir.length);
+  } while (
+    directory.length > 1 &&
+    directory.length >= options.tsconfigRootDir.length
+  );
 
   throw new Error(
-    `project was set to \`true\` but couldn't find any tsconfig.json relative to '${options.filePath}' within '${tsconfigRootDir}'.`,
+    `project was set to \`true\` but couldn't find any tsconfig.json relative to '${options.filePath}' within '${options.tsconfigRootDir}'.`,
   );
 }
