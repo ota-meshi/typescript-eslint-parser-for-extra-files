b452655a1bf0565981605de61cc07b023be8e973
yosuke ota <otameshiyo23@gmail.com>
2025-04-06 14:42:27 +0900

    update test fixtures

--- tests/fixtures/types/vue/vue-script-setup01/types.vue
+++ tests/fixtures/types/vue/vue-script-setup01/types.vue
@@ -5,11 +5,11 @@ export default {
 </script>
 
 <script setup lang="ts">
-import Foo from "../vue-script-setup02/source.vue"; // Foo: DefineComponent<{ a: { type: NumberConstructor; default: number; }; }, { foo: number; }, unknown, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, ... 5 more ..., {}>
+import Foo from "../vue-script-setup02/source.vue"; // Foo: DefineComponent<ExtractPropTypes<{ a: { type: NumberConstructor; default: number; }; }>, { foo: number; }, {}, {}, {}, ComponentOptionsMixin, ... 13 more ..., any>
 import { TypeFoo } from "../vue-script-setup02/source.vue"; // TypeFoo: any, TypeFoo: any
 import { numberValue } from "./number"; // numberValue: 1, numberValue: 1
-import { ref } from "vue"; // ref: { <T>(value: T): Ref<UnwrapRef<T>>; <T = any>(): Ref<T | undefined>; }, ref: { <T>(value: T): Ref<UnwrapRef<T>>; <T = any>(): Ref<T | undefined>; }
-let a: TypeFoo = ref(1); // a: string, ref(1): Ref<number>
+import { ref } from "vue"; // ref: { <T>(value: T): [T] extends [Ref<any, any>] ? IfAny<T, Ref<T, T>, T> : Ref<UnwrapRef<T>, T | UnwrapRef<T>>; <T = any>(): Ref<...>; }, ref: { <T>(value: T): [T] extends [Ref<any, any>] ? IfAny<T, Ref<T, T>, T> : Ref<UnwrapRef<T>, T | UnwrapRef<T>>; <T = any>(): Ref<...>; }
+let a: TypeFoo = ref(1); // a: string, ref(1): Ref<number, number>
 let b: TypeFoo; // b: string
 defineProps<{ foo: string }>(); // defineProps<{ foo: string }>(): DefineProps<LooseRequired<{ foo: string; }>, never>
 console.log(numberValue); // console.log(numberValue): void
